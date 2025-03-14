6d2aedbeed338e21954c73d0bdffa7fd46600628
jhyc256 <jhyc256@gmail.com>
2025-03-14 03:58:06 -0400

    Simple file reading done in testing

--- testing.sh
+++ testing.sh
@@ -5,13 +5,23 @@ patron_search() {
 	echo "${bold}Search a Patron Details"
 	echo
 	read -p "Enter Patron ID: " patron_id
-	{
-	declare -a input_array
-      	while IFS="," read -a input_array -u "./testing.txt" 
-        	do
-			echo "${input_array[0]},${input_array[2]}" 
-        	done
-	}
+
+	while IFS=: 
+	    read -r part1 part2 part3 part4;
+	do
+	    array=("$part1" "$part2" "$part3" "$part4")
+		
+		if [[ "$patron_id" == "$part1" ]]; then
+			echo "$part3" "$part2"
+		fi
+			    
+   	done < testing.txt
+	
+	
+	
 }
 
+
 patron_search
+
+## TASK 4 
