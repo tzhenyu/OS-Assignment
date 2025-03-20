8a2edcf12b3d2f82ef7539b816f74913420fec90
mohamed-mahir59 <mohamedmbh-wp23@student.tarc.edu.my>
2025-03-20 20:21:26 +0800

    I have added the new sort function, after this all will be easier

--- sort.sh
+++ sort.sh
@@ -12,12 +12,32 @@ sortById(){
     datas=("${patron[@]:1}")
 
     # Sort the array by PatronID (first field) using sort
-    sorted=$(printf "%s\n" "${datas[@]}" | sort -t ':' -k7)
+    sorted=$(printf "%s\n" "${datas[@]}" | sort -t ':' -k1)
 
     echo "$header"
     echo "$sorted"
 
 }
 
+sortById
 
+sortByDate(){
+    #read the file from patron into array form (the -t is to remove the new line occurence or it will mess up the indexes)
+    mapfile -t patron < patron.txt
+    
+    #a freaking header
+    header="${patron[0]}"
+
+    # Extract the data [array[@]:start:end]
+    datas=("${patron[@]:1}")
+
+    # Sort the array by PatronID (first field) using sort
+    sorted=$(printf "%s\n" "${datas[@]}" | sort -t':' -k7,7 -k7.4,7.5n -k7.7,7.10n)
+
+    echo "$header"
+    echo "$sorted"
+}
+
+echo -e "\n\n"
+sortByDate
 #how the heck should I sort using the date?
