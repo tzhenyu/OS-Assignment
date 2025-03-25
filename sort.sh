5c68fe78f6d05a14b5c2d6d9dfaee24ec3fae2b6
mohamed-mahir59 <mohamedmbh-wp23@student.tarc.edu.my>
2025-03-25 18:32:11 +0800

    new function but still have issue

--- sort.sh
+++ sort.sh
@@ -19,8 +19,6 @@ sortById(){
 
 }
 
-sortById
-
 sortByDate(){
     #read the file from patron into array form (the -t is to remove the new line occurence or it will mess up the indexes)
     mapfile -t patron < patron.txt
@@ -38,6 +36,54 @@ sortByDate(){
     echo "$sorted"
 }
 
-echo -e "\n\n"
-sortByDate
-#how the heck should I sort using the date?
+DeletePatron(){
+
+    patron_file="test.txt"
+
+    echo "DELETE PATRON DETAILS"
+    echo -n "ENTER PATRON ID: "
+    read patron_id
+
+    patron_details=$(grep "^$patron_id" "$patron_file")
+
+    if [ -z "$patron_details" ]; 
+    then
+        echo "Patron ID not found!"
+        return
+    fi
+
+    id=$(echo "$patron_details" | cut -d':' -f1)
+    fname=$(echo "$patron_details" | cut -d':' -f2)
+    lname=$(echo "$patron_details" | cut -d':' -f3)
+    mobile=$(echo "$patron_details" | cut -d':' -f4)
+    dob=$(echo "$patron_details" | cut -d':' -f5)
+    type=$(echo "$patron_details" | cut -d':' -f6)
+    joined=$(echo "$patron_details" | cut -d':' -f7)
+
+
+    echo "---------------------------------------"
+    echo " First Name: $fname"
+    echo " Last Name: $lname"
+    echo " Mobile Number: $mobile"
+    echo " Birth Date (MM-DD-YYYY): $dob"
+    echo " Membership Type: $type"
+    echo " Joined Date (MM-DD-YYYY): $joined"
+    echo "---------------------------------------"
+
+    echo -n "Are you sure you want to DELETE the above Patron Details? (y)es or (q)uit: "
+    read confirm
+
+    if [["$confirm" =~ ^[yY]$]]; #this statement got an issue on accepting input
+    then
+        #finds all details that is NOT the one that has been prompted, then save it to the temp file , later on it renames back to patron_file which is kind of overwriting
+        grep -v "^$patron_id:" "$patron_file" > temp.txt && mv temp.txt "$patron_file"
+        echo "Patron details deleted successfully."
+    else
+        echo "Action Cancelled"
+        exit 1
+    fi
+    
+    
+}
+
+DeletePatron
