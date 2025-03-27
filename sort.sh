21a31a96d2c51405874ee50b360bf23fa2686552
mohamed-mahir59 <mohamedmbh-wp23@student.tarc.edu.my>
2025-03-27 14:58:57 +0800

    second ammendment

--- sort.sh
+++ sort.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 
-sortById(){
+sortById()
+{
     #read the file from patron into array form
     mapfile -t patron < patron.txt
 
     #a freaking header
     echo -e "PatronID\tFName\tLName\tMobileNum\tBirthDate\tType\tJoinedDate"
     echo "-----------------------------------------------------------------------"
+
+    outputFile="output.txt"
 
     # Sort the array by PatronID (first field) using sort
     sorted=$(printf "%s\n" "${patron[@]:1}"| sort -t ':' -k1)
@@ -17,9 +20,41 @@ sortById(){
         echo -e "$id\t$fname\t$lname\t$mobile\t$dob\t$type\t$joined"
     done <<< $sorted
 
+    echo -e " Press (q) to return to Patron Maintenance Menu.\n"
+    echo -e "Would you like to export the report as ASCII text file? (y)es (q)uit:"
+
+    read -r response
+
+    if [ $response = "y" ] || [ $response = "Y" ];
+    then
+        echo -e "PatronID\tFName\tLName\tMobileNum\tBirthDate\tType\tJoinedDate" > $outputFile
+        echo -e "-----------------------------------------------------------------------" >>  $outputFile
+
+        # Sort the array by PatronID (first field) using sort
+        sorted=$(printf "%s\n" "${patron[@]:1}"| sort -t ':' -k1)
+
+        while IFS=':' read -r id fname lname mobile dob type joined; 
+        do
+            echo -e "$id\t$fname\t$lname\t$mobile\t$dob\t$type\t$joined" >> $outputFile
+        done <<< $sorted 
+
+        echo "Exported to $outputFile successfully."
+        main_menu
+    elif [ $response = "q"] || [ $response = "Q"];
+    then 
+        echo "Returning to Main Menu"
+        sleep -s 1500
+        main_menu;
+    else
+        echo "invalid choice, returning to main menu"
+        sleep -s 1500
+        main_menu;
+    fi
+
 }
 
-sortByDate(){
+sortByDate()
+{
     #read the file from patron into array form (the -t is to remove the new line occurence or it will mess up the indexes)
     mapfile -t patron < patron.txt
     
@@ -38,65 +73,7 @@ sortByDate(){
 }
     
 
-
-DeletePatron(){ #functioning very well (insyallah)
-
-    patron_file="test.txt"
-
-    #print out the header and prompt
-    echo -e "\n\n\t\t\t\tDelete a Patron Details"
-    echo -e "\t\t\t\t=======================\n"
-    echo -n "Enter Patron ID: "
-    read patron_id
-
-    #saves the file to a variable
-    patron_details=$(grep "^$patron_id" "$patron_file")
-
-    if [ -z "$patron_details" ]; 
-    then
-        echo "Patron ID not found!"
-        return
-    fi
-
-    id=$(echo "$patron_details" | cut -d':' -f1)
-    fname=$(echo "$patron_details" | cut -d':' -f2)
-    lname=$(echo "$patron_details" | cut -d':' -f3)
-    mobile=$(echo "$patron_details" | cut -d':' -f4)
-    dob=$(echo "$patron_details" | cut -d':' -f5)
-    type=$(echo "$patron_details" | cut -d':' -f6)
-    joined=$(echo "$patron_details" | cut -d':' -f7)
-
-
-    echo "---------------------------------------"
-    echo " First Name: $fname"
-    echo " Last Name: $lname"
-    echo " Mobile Number: $mobile"
-    echo " Birth Date (MM-DD-YYYY): $dob"
-    echo " Membership Type: $type"
-    echo " Joined Date (MM-DD-YYYY): $joined"
-    echo "---------------------------------------"
-
-    echo -n "Are you sure you want to DELETE the above Patron Details? (y)es or (q)uit: "
-    read confirm
-
-    if [ "$confirm" = "Y" ] || [ "$confirm" = "y" ]; 
-    then
-        #finds all details that is NOT the one that has been prompted, then save it to the temp file , later on it renames back to patron_file which is kind of overwriting
-        grep -v "^$patron_id:" "$patron_file" > temp.txt && mv temp.txt "$patron_file"
-        echo "Patron details deleted successfully."
-    elif [ "$confirm" = "Q" ] || [ "$confirm" = "q" ];
-    then
-        echo "Action Cancelled, Returning to the menu"
-        return
-
-    else 
-        echo "Invalid choice, returning to the menu"
-        return
-    fi
-    
-    
-}
-
+sortById
 
 #do the second phase of formatting and directly add it to assignment.sh file to make it fully functional
 
