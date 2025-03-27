d8a95025faf79f2309f18ac33412c5654917fb57
mohamed-mahir59 <mohamedmbh-wp23@student.tarc.edu.my>
2025-03-27 18:54:41 +0800

    My final ammendement for each tasks

--- assignment.sh
+++ assignment.sh
@@ -135,7 +135,7 @@ add_patron () {
 ##DELETE PATRON FUNCTION
 DeletePatron(){ #functioning very well (insyallah)
 
-    patron_file="test.txt"
+    patron_file="patron.txt"
 
     #print out the header and prompt
     echo -e "\n\n\t\t\t\tDelete a Patron Details"
@@ -189,6 +189,118 @@ DeletePatron(){ #functioning very well (insyallah)
     fi
 }
 
+##SORT USER BY ID
+sortById()
+{
+    #read the file from patron into array form
+    mapfile -t patron < patron.txt
+
+    #a freaking header
+    echo -e "PatronID\tFirst Name\tLast Name\tMobile Number\tBirth Date"
+    echo "----------------------------------------------------------------------------------"
+
+    outputFile="SortByID.txt"
+
+    # Sort the array by PatronID (first field) using sort
+    sorted=$(printf "%s\n" "${patron[@]:1}"| sort -t ':' -k1)
+
+    while IFS=':' read -r id fname lname mobile dob _ _ ; 
+    do
+        echo -e "$id\t$fname\t$lname\t$mobile\t$dob\t"
+    done <<< $sorted
+
+    echo -e " Press (q) to return to Patron Maintenance Menu.\n"
+    echo -e "Would you like to export the report as ASCII text file? (y)es (q)uit:"
+
+    read -r response
+
+    if [ $response = "y" ] || [ $response = "Y" ];
+    then
+        echo -e "PatronID\tFirst Name\tLast Name\tMobile Number\tBirth Date" > "$outputFile"
+        echo -e "----------------------------------------------------------------------------------" >>  "$outputFile"
+
+        ##Sort the array by PatronID (first field) using sort
+        sorted=$(printf "%s\n" "${patron[@]:1}"| sort -t ':' -k1 )
+
+        while IFS=':' read -r id fname lname mobile dob _ _ ; 
+        do
+            echo -e "$id\t$lname\t$fname\t$mobile\t$dob" >> "$outputFile"
+        done <<< "$sorted"
+
+        echo "Exported to $outputFile successfully."
+
+        sleep 1.5
+        
+        main_menu
+    elif [ $response = "q" ] || [ $response = "Q" ];
+    then 
+        echo "Returning to Main Menu"
+        sleep 1
+        main_menu;
+    else
+        echo "invalid choice, returning to main menu"
+        sleep 1
+        main_menu;
+    fi
+
+}
+
+##SORT BY DATE
+sortByDate()
+{
+    #read the file from patron into array form (the -t is to remove the new line occurence or it will mess up the indexes)
+    mapfile -t patron < patron.txt
+    
+     # Print the header with spacing
+    echo -e "PatronID\tFirst Name\tLast Name\tMobile Number\tBirth Date\tType\tJoined Date"
+    echo "---------------------------------------------------------------------------------------------------"
+
+    outputFile="SortByDate.txt"
+
+    # Sort the data and format it
+    sorted_data=$(printf "%s\n" "${patron[@]:1}" | sort -t':' -k7.7,7.10n -k7.4,7.5n -k7.1,7.2n )
+     
+    while IFS=':' read -r id fname lname mobile dob type joined; 
+    do
+        echo -e "$id\t$fname\t$lname\t$mobile\t$dob\t$type\t$joined"
+    done <<< $sorted_data
+
+    echo -e " Press (q) to return to Patron Maintenance Menu.\n"
+    echo -e "Would you like to export the report as ASCII text file? (y)es (q)uit:"
+
+    read -r response
+
+    if [ $response = "y" ] || [ $response = "Y" ];
+    then
+        echo -e "PatronID\tFirst Name\tLast Name\tMobile Number\tBirth Date\tType\tJoined Date" > "$outputFile"
+        echo -e "----------------------------------------------------------------------------------" >>  "$outputFile"
+
+        ##Sort the array by PatronID (first field) using sort based on MM/DD/YYYY
+        sorted_data=$(printf "%s\n" "${patron[@]:1}" | sort -t':' -k7.7,7.10n -k7.1,7.2n -k7.4,7.5n )
+
+        while IFS=':' read -r id fname lname mobile dob type joined ; 
+        do
+            echo -e "$id\t$fname\t$lname\t$mobile\t$dob\t$type\t$joined" >> "$outputFile"
+        done <<< "$sorted_data"
+
+        echo "Exported to $outputFile successfully."
+
+        sleep 1.5
+        
+        main_menu
+    elif [ $response = "q" ] || [ $response = "Q" ];
+    then 
+        echo "Returning to Main Menu"
+        sleep 1
+        main_menu
+    else
+        echo "invalid choice, returning to main menu"
+        sleep 1
+        main_menu
+    fi
+
+}
+
 ## MAIN MENU FUNCTION
 main_menu () {
     clear
@@ -222,9 +334,9 @@ main_menu () {
     L|l)
         echo "insert function here";;
     P|p)
-        echo "insert function here";;
+        sortById;;
     J|j)
-        echo "insert function here";;
+        sortByDate;;
     Q|q)
         clear;
         exit ;;
--- sort.sh
+++ sort.sh
@@ -7,17 +7,17 @@ sortById()
     mapfile -t patron < patron.txt
 
     #a freaking header
-    echo -e "PatronID\tFName\tLName\tMobileNum\tBirthDate\tType\tJoinedDate"
-    echo "-----------------------------------------------------------------------"
+    echo -e "PatronID\tFirst Name\tLast Name\tMobile Number\tBirth Date"
+    echo "----------------------------------------------------------------------------------"
 
-    outputFile="output.txt"
+    outputFile="SortByID.txt"
 
     # Sort the array by PatronID (first field) using sort
     sorted=$(printf "%s\n" "${patron[@]:1}"| sort -t ':' -k1)
 
-    while IFS=':' read -r id fname lname mobile dob type joined; 
+    while IFS=':' read -r id fname lname mobile dob _ _ ; 
     do
-        echo -e "$id\t$fname\t$lname\t$mobile\t$dob\t$type\t$joined"
+        echo -e "$id\t$fname\t$lname\t$mobile\t$dob\t"
     done <<< $sorted
 
     echo -e " Press (q) to return to Patron Maintenance Menu.\n"
@@ -27,28 +27,29 @@ sortById()
 
     if [ $response = "y" ] || [ $response = "Y" ];
     then
-        echo -e "PatronID\tFName\tLName\tMobileNum\tBirthDate\tType\tJoinedDate" > $outputFile
-        echo -e "-----------------------------------------------------------------------" >>  $outputFile
+        echo -e "PatronID\tFirst Name\tLast Name\tMobile Number\tBirth Date" > "$outputFile"
+        echo -e "----------------------------------------------------------------------------------" >>  "$outputFile"
 
-        # Sort the array by PatronID (first field) using sort
-        sorted=$(printf "%s\n" "${patron[@]:1}"| sort -t ':' -k1)
+        ##Sort the array by PatronID (first field) using sort
+        sorted=$(printf "%s\n" "${patron[@]:1}"| sort -t ':' -k1 )
 
-        while IFS=':' read -r id fname lname mobile dob type joined; 
+        while IFS=':' read -r id fname lname mobile dob _ _ ; 
         do
-            echo -e "$id\t$fname\t$lname\t$mobile\t$dob\t$type\t$joined" >> $outputFile
-        done <<< $sorted 
+            echo -e "$id\t$lname\t$fname\t$mobile\t$dob" >> "$outputFile"
+        done <<< "$sorted"
 
         echo "Exported to $outputFile successfully."
-        main_menu
-    elif [ $response = "q"] || [ $response = "Q"];
+        
+        echo "done"
+    elif [ $response = "q" ] || [ $response = "Q" ];
     then 
         echo "Returning to Main Menu"
-        sleep -s 1500
-        main_menu;
+        sleep 2
+        echo "executed";
     else
         echo "invalid choice, returning to main menu"
-        sleep -s 1500
-        main_menu;
+        sleep 2
+        echo "executed";
     fi
 
 }
@@ -59,21 +60,55 @@ sortByDate()
     mapfile -t patron < patron.txt
     
      # Print the header with spacing
-    echo -e "PatronID\tFName\tLName\tMobileNum\tBirthDate\tType\tJoinedDate"
-    echo "-----------------------------------------------------------------------"
+    echo -e "PatronID\tFirst Name\tLast Name\tMobile Number\tBirth Date\tType\tJoined Date"
+    echo "---------------------------------------------------------------------------------------------------"
+
+    outputFile="SortByDate.txt"
 
     # Sort the data and format it
-    sorted_data=$(printf "%s\n" "${patron[@]:1}" | sort -t':' -k7.7,7.10n -k7.4,7.5n -k7.1,7.2n)
+    sorted_data=$(printf "%s\n" "${patron[@]:1}" | sort -t':' -k7.7,7.10n -k7.4,7.5n -k7.1,7.2n )
      
     while IFS=':' read -r id fname lname mobile dob type joined; 
     do
         echo -e "$id\t$fname\t$lname\t$mobile\t$dob\t$type\t$joined"
     done <<< $sorted_data
 
+    echo -e " Press (q) to return to Patron Maintenance Menu.\n"
+    echo -e "Would you like to export the report as ASCII text file? (y)es (q)uit:"
+
+    read -r response
+
+    if [ $response = "y" ] || [ $response = "Y" ];
+    then
+        echo -e "PatronID\tFirst Name\tLast Name\tMobile Number\tBirth Date\tType\tJoined Date" > "$outputFile"
+        echo -e "----------------------------------------------------------------------------------" >>  "$outputFile"
+
+        ##Sort the array by PatronID (first field) using sort based on MM/DD/YYYY
+        sorted_data=$(printf "%s\n" "${patron[@]:1}" | sort -t':' -k7.7,7.10n -k7.1,7.2n -k7.4,7.5n )
+
+        while IFS=':' read -r id fname lname mobile dob type joined ; 
+        do
+            echo -e "$id\t$fname\t$lname\t$mobile\t$dob\t$type\t$joined" >> "$outputFile"
+        done <<< "$sorted_data"
+
+        echo "Exported to $outputFile successfully."
+        
+        echo "done"
+    elif [ $response = "q" ] || [ $response = "Q" ];
+    then 
+        echo "Returning to Main Menu"
+        sleep 1
+        echo "executed";
+    else
+        echo "invalid choice, returning to main menu"
+        sleep 1
+        echo "executed";
+    fi
+
 }
     
 
-sortById
+sortByDate
 
 #do the second phase of formatting and directly add it to assignment.sh file to make it fully functional
 
