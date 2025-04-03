3b11a0d25104f9dd28d8a9bcd95e51a07f3a32df
jhyc256 <jhyc256@gmail.com>
2025-04-03 11:23:08 -0400

    Task3

--- testing.sh
+++ testing.sh
@@ -1,27 +1,51 @@
 ## TASK 3 SEARCH PATRON BY ID
+
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+
+#Makes sure that working directory is same as script directory
+cd $(dirname "$0")
+
 patron_search() {
 	clear
 	
 	echo "${bold}Search a Patron Details"
-	echo
+	echo "${normal}"
 	read -p "Enter Patron ID: " patron_id
+	echo
 
-	while IFS=: 
-	    read -r part1 part2 part3 part4;
-	do
-	    array=("$part1" "$part2" "$part3" "$part4")
+	#get line where patron_id is a match
+	x=$(grep "$patron_id" ./testing.txt)
+	
+	if [[ -n "$x" ]]; then
+		#read $x and assign fields to variables x(1-7)
+		IFS=':' read -r x1 x2 x3 x4 x5 x6 x7 <<< "$x"
+		echo "First Name : $x2"
+		echo "Last Name : $x3"
+		echo "Mobile Number : $x4"          
+		echo "Birth Date (MM-DD-YYYY) : $x5"
+		echo "Membership type : $x6"
+		echo "Joined Date (MM-DD-YYYY) : $x7"
 		
-		if [[ "$patron_id" == "$part1" ]]; then
-			echo "$part3" "$part2"
-		fi
-			    
-   	done < testing.txt
-	
-	
+		echo ; echo ; echo
+		echo "Press (q) to return to Patron Maintenance Menu."
+		echo 
+		read -p "Search another patron? (y)es or (q)uit : " choicePS
+	else
+  		echo "No matching ID found."
+  		read -p "Search another patron? (y)es or (q)uit : " choicePS
+
+		case "$choice" in 
+    	Y|y)
+    		patron_search;;
+    	Q|q)
+    	##########################
+    	esac
+	fi
 	
 }
 
-
-patron_search
+patron_search #function call
 
 ## TASK 4 
